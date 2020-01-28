bbb0963ac99495dd5a5ecca817f766c672cd5d5c
Infarh <shmachilin@yandex.ru>
2020-01-28 22:17:07 +0300

    Изменение типа проектов с .NET Framework на .NET Core 3.1

--- MailSender/App.xaml.cs
+++ MailSender/App.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows;
 namespace MailSender
 {
     /// <summary>
-    /// Логика взаимодействия для App.xaml
+    /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
--- MailSender/MainWindow.xaml.cs
+++ MailSender/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ using System.Windows.Shapes;
 namespace MailSender
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
