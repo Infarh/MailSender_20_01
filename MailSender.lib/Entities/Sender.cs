e92e4476bf5aff7f9ff56ea10415006a45bcad5f
Infarh <shmachilin@yandex.ru>
2020-01-31 20:03:13 +0300

    Сущности сервера и тправителя почты

---
+++ MailSender.lib/Entities/Sender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailSender.lib.Entities
+{
+    public class Sender
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+    }
+}
---
+++ MailSender.lib/Entities/Server.cs
@@ -0,0 +1,18 @@
+﻿namespace MailSender.lib.Entities
+{
+    /// <summary>Почтовый сервер</summary>
+    public class Server
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public int Port { get; set; }
+
+        public bool UseSSL { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+    }
+}
