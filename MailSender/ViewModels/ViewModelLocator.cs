e9ae47cd1ae248eec3dcd5255e75f2c0ea355e74
Infarh <shmachilin@yandex.ru>
2020-02-04 20:27:48 +0300

    Подключение базовой нфраструктуры MVVMLight

---
+++ MailSender/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommonServiceLocator;
+using GalaSoft.MvvmLight.Ioc;
+
+
+namespace MailSender.ViewModels
+{
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            ////if (ViewModelBase.IsInDesignModeStatic)
+            ////{
+            ////    // Create design time view services and models
+            ////    SimpleIoc.Default.Register<IDataService, DesignDataService>();
+            ////}
+            ////else
+            ////{
+            ////    // Create run time view services and models
+            ////    SimpleIoc.Default.Register<IDataService, DataService>();
+            ////}
+
+            //SimpleIoc.Default.Register<MainViewModel>();
+        }
+    }
+}
