1f878a23e908694e64ef623c3439a6372ed6852a
Infarh <shmachilin@yandex.ru>
2020-02-07 20:04:50 +0300

    Переработка сервиса хранилища данных получателей писем в памяти

--- MailSender.lib/Data/TestData.cs
+++ MailSender.lib/Data/TestData.cs
@@ -10,23 +10,23 @@ namespace MailSender.lib.Data
     {
         public static List<Server> Servers { get; } = new List<Server>
         {
-            new Server { Id = 0, Name = "Яндекс", Address = "smtp.yandex.ru", Port = 587, Login = "UserLogin", Password = "Password".Encode(3) },
-            new Server { Id = 1, Name = "Mail.ru", Address = "smtp.mail.ru", Port = 587, Login = "UserLogin", Password = "Password".Encode(3) },
-            new Server { Id = 2, Name = "GMail", Address = "smtp.gmail.com", Port = 587, Login = "UserLogin", Password = "Password".Encode(3) },
+            new Server { Id = 1, Name = "Яндекс", Address = "smtp.yandex.ru", Port = 587, Login = "UserLogin", Password = "Password".Encode(3) },
+            new Server { Id = 2, Name = "Mail.ru", Address = "smtp.mail.ru", Port = 587, Login = "UserLogin", Password = "Password".Encode(3) },
+            new Server { Id = 3, Name = "GMail", Address = "smtp.gmail.com", Port = 587, Login = "UserLogin", Password = "Password".Encode(3) },
         };
 
         public static List<Sender> Senders { get; } = new List<Sender>
         {
-            new Sender { Id = 0, Name = "Иванов", Address = "ivanov@server.ru" },
-            new Sender { Id = 1, Name = "Петров", Address = "petrov@server.ru" },
-            new Sender { Id = 2, Name = "Сидоров", Address = "sidorov@server.ru" },
+            new Sender { Id = 1, Name = "Иванов", Address = "ivanov@server.ru" },
+            new Sender { Id = 2, Name = "Петров", Address = "petrov@server.ru" },
+            new Sender { Id = 3, Name = "Сидоров", Address = "sidorov@server.ru" },
         };
 
         public static List<Recipient> Recipients { get; } = new List<Recipient>
         {
-            new Recipient { Id = 0, Name = "Иванов", Address = "ivanov@server.ru" },
-            new Recipient { Id = 1, Name = "Петров", Address = "petrov@server.ru" },
-            new Recipient { Id = 2, Name = "Сидоров", Address = "sidorov@server.ru" },
+            new Recipient { Id = 1, Name = "Иванов", Address = "ivanov@server.ru" },
+            new Recipient { Id = 2, Name = "Петров", Address = "petrov@server.ru" },
+            new Recipient { Id = 3, Name = "Сидоров", Address = "sidorov@server.ru" },
         };
     }
 }
