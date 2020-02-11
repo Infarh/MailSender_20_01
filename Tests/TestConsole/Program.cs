65a1e0df7e4da0c133d80cf7ee4b3feaad352f39
Infarh <shmachilin@yandex.ru>
2020-02-11 20:13:31 +0300

    Пример создания нового потока

---
+++ Tests/TestConsole/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+namespace TestConsole
+{
+    class Program
+    {
+        private static void CheckThread(Thread thread)
+        {
+            Console.WriteLine("Поток[id:{0}]:{1} - {2}фоновый", thread.ManagedThreadId, thread.Name, thread.IsBackground ? "" : "не ");
+        }
+
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.Name = "Главный поток";
+            CheckThread(Thread.CurrentThread);
+
+           //FirstThreadMethod();
+           //var first_thread = new Thread(new ThreadStart(FirstThreadMethod));
+           var clock_thread = new Thread(ClockThread);
+            clock_thread.Name = "Фоновый потолк";
+            clock_thread.Priority = ThreadPriority.BelowNormal;
+            clock_thread.IsBackground = true;
+            clock_thread.Start();
+
+
+            Console.WriteLine("Главный поток завершился.");
+            Console.ReadLine();
+        }
+
+        private static void ClockThread()
+        {
+            CheckThread(Thread.CurrentThread);
+            while (true)
+            {
+                Console.Title = DateTime.Now.ToString();
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
