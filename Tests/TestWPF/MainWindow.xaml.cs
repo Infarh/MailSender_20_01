de1462876492ffd3bc31f47c981d36d28dc33e58
Infarh <shmachilin@yandex.ru>
2020-02-14 21:16:11 +0300

    Пример зависшего приложения

--- Tests/TestWPF/MainWindow.xaml.cs
+++ Tests/TestWPF/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
 ﻿//using TestWPF.ViewModels;
 
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
 namespace TestWPF
 {
     public partial class MainWindow
@@ -8,6 +12,34 @@ namespace TestWPF
         {
             InitializeComponent();
             //DataContext = new MainWindowViewModel();
+        }
+
+        private void OnStartButtonClick(object Sender, RoutedEventArgs E)
+        {
+            const string message = "Hello World!";
+
+            var result = GetMessageLength(message);
+
+            ResultRun.Text = result.ToString();
+        }
+
+        private void OnCancelButtonClick(object Sender, RoutedEventArgs E)
+        {
+
+        }
+
+        private Task<int> GetMessageLengthAsync(string Message, int Timeout = 30)
+        {
+            return Task.Run(() => GetMessageLength(Message, Timeout));
+        }
+
+        private int _StartCount;
+        private int GetMessageLength(string Message, int Timeout = 30)
+        {
+            for(var i = 0; i < 100; i++)
+                Thread.Sleep(Timeout);
+
+            return Message.Length + _StartCount++;
         }
     }
 }
