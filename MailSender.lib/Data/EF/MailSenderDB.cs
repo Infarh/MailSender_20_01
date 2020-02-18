90bc1d5ae1d16a638cbd2f730e4d61f4f467a4bf
Infarh <shmachilin@yandex.ru>
2020-02-18 21:26:46 +0300

    Контекст БД

---
+++ MailSender.lib/Data/EF/MailSenderDB.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MailSender.lib.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MailSender.lib.Data.EF
+{
+    public class MailSenderDB : DbContext
+    {
+        public DbSet<Mail> Mails { get; set; }
+
+        public DbSet<Sender> Senders { get; set; }
+
+        public DbSet<Recipient> Recipients { get; set; }
+
+        public DbSet<Server> Servers { get; set; }
+
+        public DbSet<MailingList> MailingLists { get; set; }
+
+        public DbSet<SchedulerTask> SchedulerTasks { get; set; }
+
+        public MailSenderDB(DbContextOptions<MailSenderDB> opt) : base(opt) { }
+    }
+}
