43cd88fd4238a790de665935c03c0b76dc122061
Infarh <shmachilin@yandex.ru>
2020-02-21 18:58:53 +0300

    Удаление хлама

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace TestConsole
 {
@@ -11,14 +6,8 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            //TPLTests.Start();
-            //TasksTest.Start();
-            //TasksTest.StartAsync();
-            TasksTest.StartDataProcessAsync();
-
-            Console.WriteLine("Главный поток завершился");
+            Console.WriteLine("!!!");
             Console.ReadLine();
-            Console.WriteLine("Приложение должно быть закрыто");
         }
     }
 }
