5b093d842217f8e1691e16c7f24683a615396cdd
Infarh <shmachilin@yandex.ru>
2020-02-21 21:04:55 +0300

    Рефлексия

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace TestConsole
 {
@@ -6,6 +9,64 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
+            //var domain = AppDomain.CreateDomain("TestDomain");
+            //domain.ExecuteAssembly(Assembly.GetEntryAssembly().Location);
+
+            //Assembly
+            //AssemblyName
+            //Module
+
+            //Type
+
+            //MemberInfo
+            ////FieldInfo
+            ////PropertyInfo
+            ////ConstructorInfo
+            ////EventInfo
+            ////MethodInfo
+            //////ParameterInfo
+
+            var lib_file = new FileInfo("TestLib.dll");
+
+            var lib = Assembly.LoadFile(lib_file.FullName);
+
+            var printer_type = lib.GetType("TestLib.Printer");
+
+            //foreach (var method in printer_type.GetMethods())
+            //{
+            //    Console.WriteLine("{0} {1}({2})",
+            //        method.ReturnType.Name,
+            //        method.Name,
+            //        string.Join(", ", method.GetParameters().Select(p => $"{p.ParameterType.Name} {p.Name}")));
+            //}
+
+            var printer_ctor = printer_type.GetConstructor(new[] { typeof(string) });
+
+            var printer = printer_ctor.Invoke(new object[] { ">>>>>" });
+
+            var print_method_info = printer_type.GetMethod("Print", BindingFlags.Instance | BindingFlags.Public);
+
+            print_method_info.Invoke(printer, new object[] { "Hello World!" });
+
+            var printer_prefix = printer_type.GetField("_Prefix", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            var prefix_value = (string) printer_prefix.GetValue(printer);
+            prefix_value += "|||||";
+
+            printer_prefix.SetValue(printer, prefix_value);
+
+            var internal_printer_type = lib.GetType("TestLib.InternalPrinter");
+
+            var internal_printer_ctor = internal_printer_type.GetConstructor(Array.Empty<Type>());
+
+            var internal_printer = internal_printer_ctor.Invoke(Array.Empty<object>());
+
+            var internal_print_method_info = internal_printer_type.GetMethod("Print", BindingFlags.Instance | BindingFlags.Public);
+
+            internal_print_method_info.Invoke(internal_printer, new object[] { "Hello World!" });
+
+            
+
             Console.WriteLine("!!!");
             Console.ReadLine();
         }
---
+++ Tests/TestLib/Printer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace TestLib
+{
+    public class Printer
+    {
+        private string _Prefix;
+
+        public Printer(string Prefix) => _Prefix = Prefix;
+
+        public virtual void Print([Description("Печатаемое сообщение")] string str) => 
+            Console.WriteLine("{0}{1}", _Prefix, str);
+    }
+}
